56a7f198ceb0e0032a527ce348a59aa3bf944722
firstdraft <81296586+s34azuread@users.noreply.github.com>
2021-12-25 10:40:37 +0530

    Added empty caller activity.

---
+++ draw_retrieve_caller/src/main/java/org/firstdraft/draw_retrieve_caller/draw_retrieve_caller.java
@@ -0,0 +1,14 @@
+package org.firstdraft.draw_retrieve_caller;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class draw_retrieve_caller extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_draw_retrieve_caller);
+    }
+}
